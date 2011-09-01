da3cff9eb579becefa6cc305c41d0d18d7a6d023
Ricardo <tecnovirtu@gmail.com>
2011-08-31 17:03:46 -0700

    Fixed tags error

--- examples/basic.php
+++ examples/basic.php
@@ -4,16 +4,14 @@ require '../includes/identit.class.php';
 
 // Set a main text
 $text = '
-<?php
 if ($foo == $bar) {
-echo "Foo its equal to bar<br />";
+echo "Foo its equal to bar";
 if ($foo = $baz) {
 echo "Foo its equal to baz";
 }
 }else{
 echo "Foo is not equal to bar";
-}
-?>';
+}';
 
 $bar = new identIT();
 echo '<pre>'.$bar->ident($text, true).'</pre>';
